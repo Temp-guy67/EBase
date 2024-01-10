695ef8cf720462d196d405171c5ea75049cd80e4
Temp-guy67 <profiletemp66@gmail.com>
2024-01-10 18:47:30 +0530

    Upto today

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-from fastapi import Depends, FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPBearer, APIKeyHeader
-from host_app.common.middle_ware import RateLimiterMiddleware
-from host_app.common.token_bucket import TokenBucket
-from services import onStartService
-from host_app.database.database import Base, engine
 import logging, prop_file
-from host_app.routes.user_routes import user_router
-from host_app.routes.order_routes import order_router
+from fastapi import FastAPI
+from services import onStartService
+from fastapi.middleware.cors import CORSMiddleware
+from host_app.database.database import Base, engine
 from host_app.routes.auth_routes import auth_router
+from host_app.routes.user_routes import user_router
+from host_app.common.token_bucket import TokenBucket
+from host_app.routes.order_routes import order_router
 from host_app.routes.public_routes import public_router
 from host_app.routes.services_routes import service_router
-from host_app.logs import log_manager
+from host_app.common.middle_ware import RateLimiterMiddleware
 
 app = FastAPI(title=prop_file.TITLE, summary=prop_file.SUMMARY, description=prop_file.DESCRIPTION, version=prop_file.VERSION, openapi_tags=prop_file.TAGS_METADATA, redoc_url=None)
 
@@ -42,19 +40,10 @@ app.add_middleware(RateLimiterMiddleware, bucket=bucket)
 async def startService():
     await onStartService()
 
-security = HTTPBearer()
-api_he = APIKeyHeader(name="api_key")
-
-
 
 @app.get("/")
 async def hello():
-    return {"message" : "Entry Screen"}
-
-@app.get("/logs")
-async def read_logs():
-    res = await log_manager.read_logs()
-    return res
+    return {"message" : "Application loaded successfully"}
 
 
 @app.get("/test/login")
@@ -66,16 +55,6 @@ async def login_test():
     except Exception as ex :
         logging.exception("[main][Exception in signup] {} ".format(ex))
 
-
-@app.get("/test/getuser")
-async def get_user():
-    return {
-        "email": "test3@testmail.com",
-        "created_time": "2023-09-18T12:30:48",
-        "id": 1,
-        "username": "test3",
-        "phone": "123456711",
-    }
 
 
 
